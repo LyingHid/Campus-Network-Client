10faf30eedf88345b91b53315ae3c7d59d5969f7
Glove An <GloveInTheHouse@gmail.com>
2017-08-20 17:35:20 +0800

    using macros to create python object

---
+++ packets/ruijie/extra_hash.h
@@ -0,0 +1,283 @@
+#ifndef HEADER_EXTRA_HASH
+#define HEADER_EXTRA_HASH
+
+
+/** SECTION: libTomCrypt start **/
+/* the MD5 implementation is modified from libTomCrypt
+ * http://www.libtom.net/LibTomCrypt/ */
+
+
+/* ulong64: 64-bit data type */
+#define CONST64(n) n ## ULL
+
+
+/* rotate the hard way (platform optimizations could be done) */
+#define ROLc(x, y) ( (((unsigned long)(x)<<(unsigned long)((y)&31)) | (((unsigned long)(x)&0xFFFFFFFFUL)>>(unsigned long)(32-((y)&31)))) & 0xFFFFFFFFUL)
+
+
+/* Endian Neutral macros that work on all platforms */
+#define STORE32L(x, y)                                                                     \
+     { (y)[3] = (unsigned char)(((x)>>24)&255); (y)[2] = (unsigned char)(((x)>>16)&255);   \
+       (y)[1] = (unsigned char)(((x)>>8)&255); (y)[0] = (unsigned char)((x)&255); }
+
+#define LOAD32L(x, y)                            \
+     { x = ((unsigned long)((y)[3] & 255)<<24) | \
+           ((unsigned long)((y)[2] & 255)<<16) | \
+           ((unsigned long)((y)[1] & 255)<<8)  | \
+           ((unsigned long)((y)[0] & 255)); }
+
+#define LOAD64H(x, y)                                                         \
+    { x = (((uint64_t)((y)[0] & 255))<<56)|(((uint64_t)((y)[1] & 255))<<48) | \
+          (((uint64_t)((y)[2] & 255))<<40)|(((uint64_t)((y)[3] & 255))<<32) | \
+          (((uint64_t)((y)[4] & 255))<<24)|(((uint64_t)((y)[5] & 255))<<16) | \
+          (((uint64_t)((y)[6] & 255))<<8)|(((uint64_t)((y)[7] & 255))); }
+
+#define STORE64L(x, y)                                                                     \
+     { (y)[7] = (unsigned char)(((x)>>56)&255); (y)[6] = (unsigned char)(((x)>>48)&255);   \
+       (y)[5] = (unsigned char)(((x)>>40)&255); (y)[4] = (unsigned char)(((x)>>32)&255);   \
+       (y)[3] = (unsigned char)(((x)>>24)&255); (y)[2] = (unsigned char)(((x)>>16)&255);   \
+       (y)[1] = (unsigned char)(((x)>>8)&255); (y)[0] = (unsigned char)((x)&255); }
+
+#define STORE64H(x, y)                                                                     \
+    { (y)[0] = (unsigned char)(((x)>>56)&255); (y)[1] = (unsigned char)(((x)>>48)&255);    \
+      (y)[2] = (unsigned char)(((x)>>40)&255); (y)[3] = (unsigned char)(((x)>>32)&255);    \
+      (y)[4] = (unsigned char)(((x)>>24)&255); (y)[5] = (unsigned char)(((x)>>16)&255);    \
+      (y)[6] = (unsigned char)(((x)>>8)&255); (y)[7] = (unsigned char)((x)&255); }
+
+
+/* a simple macro for making hash "process" functions */
+#define HASH_PROCESS(func_name, compress_name, state_name, block_size)          \
+void func_name(state_name *md, const unsigned char *in, unsigned long inlen)    \
+{                                                                               \
+    unsigned long n;                                                            \
+                                                                                \
+    assert(md != NULL);                                                         \
+    assert(in != NULL);                                                         \
+    assert(md->curlen <= sizeof(md->buf));                                      \
+                                                                                \
+    while (inlen > 0)                                                           \
+    {                                                                           \
+        if (md->curlen == 0 && inlen >= block_size)                             \
+        {                                                                       \
+            compress_name(md, (unsigned char *)in);                             \
+            md->length += block_size * 8;                                       \
+            in         += block_size;                                           \
+            inlen      -= block_size;                                           \
+        }                                                                       \
+        else                                                                    \
+        {                                                                       \
+            n = Py_MIN(inlen, (Py_ssize_t)(block_size - md->curlen));           \
+            memcpy(md->buf + md->curlen, in, (size_t)n);                        \
+            md->curlen += n;                                                    \
+            in         += n;                                                    \
+            inlen      -= n;                                                    \
+            if (md->curlen == block_size)                                       \
+            {                                                                   \
+                compress_name(md, md->buf);                                     \
+                md->length += 8 * block_size;                                   \
+                md->curlen  = 0;                                                \
+            }                                                                   \
+        }                                                                       \
+    }                                                                           \
+}
+
+
+/** SECTION: libTomCrypt end **/
+/** SECTION: Python Class start **/
+/* C API learned from CPython md5module.c and documents
+ * https://github.com/python/cpython/blob/master/Modules/md5module.c
+ * https://docs.python.org/3/extending/extending.html
+ * https://docs.python.org/3/extending/newtypes.html */
+
+
+ /*
+  * Given a PyObject* obj, fill in the Py_buffer* viewp with the result
+  * of PyObject_GetBuffer.  Sets an exception and issues the erraction
+  * on any errors, e.g. 'return NULL' or 'goto error'.
+  */
+ #define GET_BUFFER_VIEW_OR_ERROR(obj, viewp, erraction) do { \
+         if (PyUnicode_Check((obj))) { \
+             PyErr_SetString(PyExc_TypeError, \
+                             "Unicode-objects must be encoded before hashing");\
+             erraction; \
+         } \
+         if (!PyObject_CheckBuffer((obj))) { \
+             PyErr_SetString(PyExc_TypeError, \
+                             "object supporting the buffer API required"); \
+             erraction; \
+         } \
+         if (PyObject_GetBuffer((obj), (viewp), PyBUF_SIMPLE) == -1) { \
+             erraction; \
+         } \
+         if ((viewp)->ndim > 1) { \
+             PyErr_SetString(PyExc_BufferError, \
+                             "Buffer must be single dimension"); \
+             PyBuffer_Release((viewp)); \
+             erraction; \
+         } \
+     } while(0)
+
+ #define GET_BUFFER_VIEW_OR_ERROUT(obj, viewp) \
+ GET_BUFFER_VIEW_OR_ERROR(obj, viewp, return NULL)
+
+
+#define OBJECT_DEF(tom) \
+    typedef struct { \
+        PyObject_HEAD \
+        /* Type-specific fields go here. */ \
+        tom ## _state hash_state; \
+    } HashObject; \
+
+#define OBJECT_NEW() \
+    static PyObject *hash_new(PyTypeObject *type, PyObject *args, PyObject *kwds) \
+    { \
+        return type->tp_alloc(type, 0); \
+    }
+
+#define OBJECT_INIT(tom) \
+static int hash_init(HashObject *self, PyObject *args, PyObject *kwds) \
+    { \
+        Py_buffer buffer = {0}; \
+ \
+        PyArg_ParseTuple(args, "|y*", &buffer); \
+\
+        tom ## _init(&self->hash_state); \
+        if(buffer.buf) \
+        { \
+            tom ## _process(&self->hash_state, buffer.buf, buffer.len); \
+            PyBuffer_Release(&buffer); \
+        } \
+ \
+        return 0; \
+    }
+
+#define OBJECT_DEL() \
+    static void hash_del(PyObject *self) \
+    { \
+        Py_TYPE(self)->tp_free(self); \
+    }
+
+#define OBJECT_DIGESTSIZE() \
+    static PyObject *hash_digestsize(PyObject *self, void *closure) \
+    { \
+        return PyLong_FromLong(DIGESTSIZE); \
+    }
+
+#define OBJECT_BLOCKSIZE() \
+    static PyObject *hash_blocksize(PyObject *self, void *closure) \
+    { \
+        return PyLong_FromLong(BLOCKSIZE); \
+    } \
+
+#define OBJECT_NAME(tom) \
+    static PyObject *hash_name(PyObject *self, void *closure) \
+    { \
+        return PyUnicode_FromString("ruijie_" #tom); \
+    }
+
+#define OBJECT_UPDATE(tom) \
+    static PyObject *hash_update(HashObject *self, PyObject *object) \
+    { \
+        Py_buffer buffer; \
+ \
+        GET_BUFFER_VIEW_OR_ERROUT(object, &buffer); \
+        tom ## _process(&self->hash_state, buffer.buf, buffer.len); \
+        PyBuffer_Release(&buffer); \
+ \
+        Py_RETURN_NONE; \
+    }
+
+#define OBJECT_DIGEST(tom) \
+    static PyObject *hash_digest(HashObject *self) \
+    { \
+        unsigned char digest[DIGESTSIZE]; \
+        tom ## _state temp; \
+ \
+        temp = self->hash_state; \
+        tom ## _done(&temp, digest); \
+        return PyBytes_FromStringAndSize((const char *)digest, DIGESTSIZE); \
+    }
+
+#define OBJECT_HEXDIGEST(tom) \
+    static PyObject *hash_hexdigest(HashObject *self) \
+    { \
+        unsigned char digest[DIGESTSIZE]; \
+        tom ## _state temp; \
+ \
+        /* Get the raw (binary) digest value */ \
+        temp = self->hash_state; \
+        tom ## _done(&temp, digest); \
+ \
+        return _Py_strhex((const char*)digest, DIGESTSIZE); \
+    }
+
+#define OBJECT_COPY() \
+    static PyObject *hash_copy(HashObject *self) \
+    { \
+        HashObject *copy = (HashObject *)Py_TYPE(self)->tp_alloc(Py_TYPE(self), 0); \
+        if (copy == NULL) return NULL; \
+ \
+        copy->hash_state = self->hash_state; \
+        return (PyObject *)copy; \
+    }
+
+#define OBJECT_GETSETER_DEF()                                       \
+    static PyGetSetDef hash_getseters[] = {                         \
+        {"digest_size", (getter)hash_digestsize, NULL, NULL, NULL}, \
+        {"block_size" , (getter)hash_blocksize , NULL, NULL, NULL}, \
+        {"name"       , (getter)hash_name      , NULL, NULL, NULL}, \
+        {NULL}  /* Sentinel */                                      \
+    };
+
+#define OBJECT_METHODS_DEF()                                           \
+    static PyMethodDef hash_methods[] = {                              \
+        {"update",    (PyCFunction)hash_update,    METH_O,      NULL}, \
+        {"digest",    (PyCFunction)hash_digest,    METH_NOARGS, NULL}, \
+        {"hexdigest", (PyCFunction)hash_hexdigest, METH_NOARGS, NULL}, \
+        {"copy",      (PyCFunction)hash_copy,      METH_NOARGS, NULL}, \
+        {NULL}         /* sentinel */                                  \
+    };
+
+#define OBJECT_TYPE_DEF(name)                \
+    static PyTypeObject type_object = {      \
+        PyVarObject_HEAD_INIT(NULL, 0)       \
+        .tp_name      = "extra." #name,      \
+        .tp_basicsize = sizeof(HashObject),  \
+        .tp_dealloc   = hash_del,            \
+        .tp_flags     = Py_TPFLAGS_DEFAULT,  \
+        .tp_methods   = hash_methods,        \
+        .tp_getset    = hash_getseters,      \
+        .tp_init      = (initproc)hash_init, \
+        .tp_new       = hash_new             \
+    };
+
+#define OBJECT_REGISTER(name, tom) \
+    PyObject *ruijie_ ## tom ## _register(PyObject *module) \
+    { \
+        if (PyType_Ready(&type_object) < 0) \
+            return NULL; \
+ \
+        Py_INCREF(&type_object); \
+        PyModule_AddObject(module, #name, (PyObject *)&type_object); \
+ \
+        return module; \
+    }
+
+#define PYTHON_OBJECT(name, tom) \
+    OBJECT_DEF(tom)              \
+    OBJECT_NEW()                 \
+    OBJECT_INIT(tom)             \
+    OBJECT_DEL()                 \
+    OBJECT_DIGESTSIZE()          \
+    OBJECT_BLOCKSIZE()           \
+    OBJECT_NAME(tom)             \
+    OBJECT_UPDATE(tom)           \
+    OBJECT_DIGEST(tom)           \
+    OBJECT_HEXDIGEST(tom)        \
+    OBJECT_COPY()                \
+    OBJECT_GETSETER_DEF()        \
+    OBJECT_METHODS_DEF()         \
+    OBJECT_TYPE_DEF(name)        \
+    OBJECT_REGISTER(name, tom)
+
+#endif
--- packets/ruijie/extra_md5.c
+++ packets/ruijie/extra_md5.c
@@ -2,43 +2,22 @@
 #include <stdint.h>
 #include <Python.h>
 #include <pystrhex.h>
+#include "extra_hash.h"
 
 
 /** SECTION: libTomCrypt start **/
 /* the MD5 implementation is modified from libTomCrypt
  * http://www.libtom.net/LibTomCrypt/ */
 
-/* rotate the hard way (platform optimizations could be done) */
-#define ROLc(x, y) ( (((unsigned long)(x)<<(unsigned long)((y)&31)) | (((unsigned long)(x)&0xFFFFFFFFUL)>>(unsigned long)(32-((y)&31)))) & 0xFFFFFFFFUL)
 
-
-/* Endian Neutral macros that work on all platforms */
-#define STORE32L(x, y)                                                                     \
-     { (y)[3] = (unsigned char)(((x)>>24)&255); (y)[2] = (unsigned char)(((x)>>16)&255);   \
-       (y)[1] = (unsigned char)(((x)>>8)&255); (y)[0] = (unsigned char)((x)&255); }
-
-#define LOAD32L(x, y)                            \
-     { x = ((unsigned long)((y)[3] & 255)<<24) | \
-           ((unsigned long)((y)[2] & 255)<<16) | \
-           ((unsigned long)((y)[1] & 255)<<8)  | \
-           ((unsigned long)((y)[0] & 255)); }
-
-#define STORE64L(x, y)                                                                     \
-     { (y)[7] = (unsigned char)(((x)>>56)&255); (y)[6] = (unsigned char)(((x)>>48)&255);   \
-       (y)[5] = (unsigned char)(((x)>>40)&255); (y)[4] = (unsigned char)(((x)>>32)&255);   \
-       (y)[3] = (unsigned char)(((x)>>24)&255); (y)[2] = (unsigned char)(((x)>>16)&255);   \
-       (y)[1] = (unsigned char)(((x)>>8)&255); (y)[0] = (unsigned char)((x)&255); }
-
-
-#define MD5_BLOCKSIZE  64
-#define MD5_DIGESTSIZE 16
+#define BLOCKSIZE  64
+#define DIGESTSIZE 16
 
 
 #define F(x,y,z)  (z ^ (x & (y ^ z)))
 #define G(x,y,z)  (y ^ (z & (y ^ x)))
 #define H(x,y,z)  (x^y^z)
 #define I(x,y,z)  (y^(x|(~z)))
-
 
 #define FF(a,b,c,d,M,s,t) \
     a = (a + F(b,c,d) + M + t); a = ROLc(a, s) + b + 2;
@@ -57,7 +36,7 @@ typedef struct _md5_state {
 } md5_state;
 
 
-static void md5_compress(md5_state *md5, unsigned char *buf)
+static void md5_compress(md5_state *md, unsigned char *buf)
 {
     uint32_t i, W[16], a, b, c, d;
 
@@ -66,10 +45,10 @@ static void md5_compress(md5_state *md5, unsigned char *buf)
         LOAD32L(W[i], buf + (4*i));
 
     /* copy state */
-    a = md5->state[0];
-    b = md5->state[1];
-    c = md5->state[2];
-    d = md5->state[3];
+    a = md->state[0];
+    b = md->state[1];
+    c = md->state[2];
+    d = md->state[3];
 
     FF(a,b,c,d,W[ 0], 7,0xd76aa478UL)
     FF(d,a,b,c,W[ 1],12,0xe8c7b756UL)
@@ -136,291 +115,85 @@ static void md5_compress(md5_state *md5, unsigned char *buf)
     II(c,d,a,b,W[ 2],15,0x2ad7d2bbUL)
     II(b,c,d,a,W[ 9],19,0xeb866391UL)  // II(b,c,d,a,W[ 9],21,0xeb86d391UL)
 
-    md5->state[0] = md5->state[0] + a;
-    md5->state[1] = md5->state[1] + b;
-    md5->state[2] = md5->state[2] + c;
-    md5->state[3] = md5->state[3] + d;
+    md->state[0] = md->state[0] + a;
+    md->state[1] = md->state[1] + b;
+    md->state[2] = md->state[2] + c;
+    md->state[3] = md->state[3] + d;
 }
+
 
 /**
    Initialize the hash state
-   @param md5  The hash state you wish to initialize
+   @param md  The hash state you wish to initialize
 */
-void md5_init(md5_state *md5)
+void md5_init(md5_state *md)
 {
-   assert(md5 != NULL);
-   md5->state[0] = 0x50137246UL;  // 0x67452301UL;
-   md5->state[1] = 0x8acf9dbeUL;  // 0xefcdab89UL;
-   md5->state[2] = 0xc9efacedUL;  // 0x98badcfeUL;
-   md5->state[3] = 0x25647013UL;  // 0x10325476UL;
-   md5->curlen = 0;
-   md5->length = 0;
+    assert(md != NULL);
+    md->state[0] = 0x50137246UL;  // 0x67452301UL;
+    md->state[1] = 0x8acf9dbeUL;  // 0xefcdab89UL;
+    md->state[2] = 0xc9efacedUL;  // 0x98badcfeUL;
+    md->state[3] = 0x25647013UL;  // 0x10325476UL;
+    md->curlen = 0;
+    md->length = 0;
 }
+
 
 /**
    Process a block of memory though the hash
-   @param md5    The hash state
+   @param md     The hash state
    @param in     The data to hash
    @param inlen  The length of the data (octets)
 */
-void md5_process(md5_state *md5, const unsigned char *in, unsigned long inlen)
-{
-    unsigned long n;
-
-    assert(md5 != NULL);
-    assert(in  != NULL);
-    assert(md5->curlen <= sizeof(md5->buf));
-
-    while(inlen > 0)
-    {
-        if (md5->curlen == 0 && inlen >= MD5_BLOCKSIZE)
-        {
-            md5_compress(md5, (unsigned char *)in);
-            md5->length += MD5_BLOCKSIZE * 8;
-            in          += MD5_BLOCKSIZE;
-            inlen       -= MD5_BLOCKSIZE;
-        }
-        else
-        {
-            n = Py_MIN(inlen, (Py_ssize_t)(MD5_BLOCKSIZE - md5->curlen));
-            memcpy(md5->buf + md5->curlen, in, (size_t)n);
-            md5->curlen += n;
-            in          += n;
-            inlen       -= n;
-            if (md5->curlen == MD5_BLOCKSIZE)
-            {
-                md5_compress(md5, md5->buf);
-                md5->length += 8 * MD5_BLOCKSIZE;
-                md5->curlen  = 0;
-            }
-        }
-    }
-}
+HASH_PROCESS(md5_process, md5_compress, md5_state, BLOCKSIZE)
 
 
 /**
    Terminate the hash to get the digest
-   @param md5  The hash state
+   @param md        The hash state
    @param out [out] The destination of the hash (16 bytes)
    @return CRYPT_OK if successful
 */
-void md5_done(md5_state *md5, unsigned char *out)
+void md5_done(md5_state *md, unsigned char *out)
 {
     int i;
 
-    assert(md5 != NULL);
+    assert(md  != NULL);
     assert(out != NULL);
-    assert(md5->curlen < sizeof(md5->buf));
+    assert(md->curlen < sizeof(md->buf));
 
     /* increase the length of the message */
-    md5->length += md5->curlen * 8;
+    md->length += md->curlen * 8;
 
     /* append the '1' bit */
-    md5->buf[md5->curlen++] = (unsigned char)0x80;
+    md->buf[md->curlen++] = (unsigned char)0x80;
 
     /* if the length is currently above 56 bytes we append zeros
      * then compress.  Then we can fall back to padding zeros and length
      * encoding like normal.
      */
-    if (md5->curlen > 56)
+    if (md->curlen > 56)
     {
-        while (md5->curlen < 64)
-            md5->buf[md5->curlen++] = (unsigned char)0;
-        md5_compress(md5, md5->buf);
-        md5->curlen = 0;
+        while (md->curlen < 64)
+            md->buf[md->curlen++] = (unsigned char)0;
+        md5_compress(md, md->buf);
+        md->curlen = 0;
     }
 
     /* pad upto 56 bytes of zeroes */
-    while (md5->curlen < 56)
-        md5->buf[md5->curlen++] = (unsigned char)0;
+    while (md->curlen < 56)
+        md->buf[md->curlen++] = (unsigned char)0;
 
     /* store length */
-    STORE64L(md5->length, md5->buf+56);
-    md5_compress(md5, md5->buf);
+    STORE64L(md->length, md->buf+56);
+    md5_compress(md, md->buf);
 
     /* copy output */
     for (i = 0; i < 4; i++)
-        STORE32L(md5->state[i], out+(4*i));
+        STORE32L(md->state[i], out+(4*i));
 }
+
 
 /** SECTION: libTomCrypt end **/
-/** SECTION: Python Class start **/
-/* C API learned from CPython md5module.c and documents
- * https://github.com/python/cpython/blob/master/Modules/md5module.c
- * https://docs.python.org/3/extending/extending.html
- * https://docs.python.org/3/extending/newtypes.html */
-
-/*
- * Given a PyObject* obj, fill in the Py_buffer* viewp with the result
- * of PyObject_GetBuffer.  Sets an exception and issues the erraction
- * on any errors, e.g. 'return NULL' or 'goto error'.
- */
-#define GET_BUFFER_VIEW_OR_ERROR(obj, viewp, erraction) do { \
-        if (PyUnicode_Check((obj))) { \
-            PyErr_SetString(PyExc_TypeError, \
-                            "Unicode-objects must be encoded before hashing");\
-            erraction; \
-        } \
-        if (!PyObject_CheckBuffer((obj))) { \
-            PyErr_SetString(PyExc_TypeError, \
-                            "object supporting the buffer API required"); \
-            erraction; \
-        } \
-        if (PyObject_GetBuffer((obj), (viewp), PyBUF_SIMPLE) == -1) { \
-            erraction; \
-        } \
-        if ((viewp)->ndim > 1) { \
-            PyErr_SetString(PyExc_BufferError, \
-                            "Buffer must be single dimension"); \
-            PyBuffer_Release((viewp)); \
-            erraction; \
-        } \
-    } while(0)
-
-#define GET_BUFFER_VIEW_OR_ERROUT(obj, viewp) \
-GET_BUFFER_VIEW_OR_ERROR(obj, viewp, return NULL)
 
 
-static PyTypeObject ruijie_md5_type;
-
-
-typedef struct {
-    PyObject_HEAD
-    /* Type-specific fields go here. */
-    md5_state md5;
-} RuijieMD5;
-
-
-static PyObject *ruijie_md5_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
-{
-    return type->tp_alloc(type, 0);
-}
-
-
-static int ruijie_md5_init(RuijieMD5 *self, PyObject *args, PyObject *kwds)
-{
-    Py_buffer buffer = {0};
-
-    PyArg_ParseTuple(args, "|y*", &buffer);
-
-    md5_init(&self->md5);
-    if(buffer.buf)
-    {
-        md5_process(&self->md5, buffer.buf, buffer.len);
-        PyBuffer_Release(&buffer);
-    }
-
-    return 0;
-}
-
-
-static void ruijie_md5_del(PyObject *self)
-{
-    Py_TYPE(self)->tp_free(self);
-}
-
-
-static PyObject *ruijie_md5_digestsize(PyObject *self, void *closure)
-{
-    return PyLong_FromLong(MD5_DIGESTSIZE);
-}
-
-
-static PyObject *ruijie_md5_blocksize(PyObject *self, void *closure)
-{
-    return PyLong_FromLong(MD5_BLOCKSIZE);
-}
-
-
-static PyObject *ruijie_md5_name(PyObject *self, void *closure)
-{
-    return PyUnicode_FromString("ruijie_md5");
-}
-
-
-static PyObject *ruijie_md5_update(RuijieMD5 *self, PyObject *object)
-{
-    Py_buffer buffer;
-
-    GET_BUFFER_VIEW_OR_ERROUT(object, &buffer);
-    md5_process(&self->md5, buffer.buf, buffer.len);
-    PyBuffer_Release(&buffer);
-
-    Py_RETURN_NONE;
-}
-
-
-static PyObject *ruijie_md5_digest(RuijieMD5 *self)
-{
-    unsigned char digest[MD5_DIGESTSIZE];
-    md5_state     temp;
-
-    temp = self->md5;
-    md5_done(&temp, digest);
-    return PyBytes_FromStringAndSize((const char *)digest, MD5_DIGESTSIZE);
-}
-
-
-static PyObject *ruijie_md5_hexdigest(RuijieMD5 *self)
-{
-    unsigned char digest[MD5_DIGESTSIZE];
-    md5_state     temp;
-
-    /* Get the raw (binary) digest value */
-    temp = self->md5;
-    md5_done(&temp, digest);
-
-    return _Py_strhex((const char*)digest, MD5_DIGESTSIZE);
-}
-
-
-static PyObject *ruijie_md5_copy(RuijieMD5 *self)
-{
-    RuijieMD5 *copy = (RuijieMD5 *)Py_TYPE(self)->tp_alloc(Py_TYPE(self), 0);
-    if (copy == NULL) return NULL;
-
-    copy->md5 = self->md5;
-    return (PyObject *)copy;
-}
-
-
-static PyGetSetDef ruijie_md5_getseters[] = {
-    {"digest_size", (getter)ruijie_md5_digestsize, NULL, NULL, NULL},
-    {"block_size" , (getter)ruijie_md5_blocksize , NULL, NULL, NULL},
-    {"name"       , (getter)ruijie_md5_name      , NULL, NULL, NULL},
-    {NULL}  /* Sentinel */
-};
-
-static PyMethodDef ruijei_md5_methods[] = {
-    {"update",    (PyCFunction)ruijie_md5_update,    METH_O,      NULL},
-    {"digest",    (PyCFunction)ruijie_md5_digest,    METH_NOARGS, NULL},
-    {"hexdigest", (PyCFunction)ruijie_md5_hexdigest, METH_NOARGS, NULL},
-    {"copy",      (PyCFunction)ruijie_md5_copy,      METH_NOARGS, NULL},
-    {NULL}         /* sentinel */
-};
-
-static PyTypeObject ruijie_md5_type = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name      = "extra.RuijieMD5",
-    .tp_basicsize = sizeof(RuijieMD5),
-    .tp_dealloc   = ruijie_md5_del,
-    .tp_flags     = Py_TPFLAGS_DEFAULT,
-    .tp_methods   = ruijei_md5_methods,
-    .tp_getset    = ruijie_md5_getseters,
-    .tp_init      = (initproc)ruijie_md5_init,
-    .tp_new       = ruijie_md5_new
-};
-
-
-PyObject *ruijie_md5_register(PyObject *module)
-{
-    if (PyType_Ready(&ruijie_md5_type) < 0)
-        return NULL;
-
-    Py_INCREF(&ruijie_md5_type);
-    PyModule_AddObject(module, "RuijieMD5", (PyObject *)&ruijie_md5_type);
-
-    return module;
-}
-
-/** SECTION: Python Class end **/
+PYTHON_OBJECT(RuijieMD5, md5)
