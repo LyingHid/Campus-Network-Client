e1ddf299d019511f911a94b2779f6fc9b575b9ac
Glove An <GloveInTheHouse@gmail.com>
2017-08-29 17:17:32 +0800

    eventloop implement timer

--- eventloop.py
+++ eventloop.py
@@ -22,7 +22,7 @@ class FileWatcher():
 class TimeWatcher():
     def __init__(self, first, repeat, callback, data=None):
         self.first = first
-        self.repeat = repeat
+        self.repeat = repeat  # one pass timer when 'repeat == 0'
         self.callback = callback
         self.data = data
         self.eventloop = None
@@ -33,7 +33,6 @@ class Eventloop():
     def __init__(self):
         self.goon = True
         self.selector = selectors.DefaultSelector()
-        self.files = {}
         self.timers = []
 
 
@@ -42,19 +41,66 @@ class Eventloop():
 
         if isinstance(watcher, FileWatcher):
             self.selector.register(watcher.file, watcher.events, watcher)
+        elif isinstance(watcher, TimeWatcher):
+            watcher.timestamp = watcher.first + time.monotonic()
+            heapq.heappush(self.timers, (watcher.timestamp, watcher))
+
+
+    def modify(self, watcher):
+        if isinstance(watcher, FileWatcher):
+            self.selector.modify(watcher.file, watcher.events, watcher)
+        elif isinstance(watcher, TimeWatcher):
+            # only 'repeat' member can be changed
+            # it will not take effect until it is popped out from the heap
+            # and re-pushed into the heap
+            pass
 
 
     def unregister(self, watcher):
         watcher.eventloop = None
 
         if isinstance(watcher, FileWatcher):
-            self.selector.unregister(watcher.file)
+            try:
+                self.selector.unregister(watcher.file)
+            except KeyError:
+                pass
+            except ValueError:
+                pass
+        elif isinstance(watcher, TimeWatcher):
+            try:
+                # using flag to indicate deletion
+                i = self.timers.index((watcher.timestamp, watcher))
+                self.timers[i] = (watcher.timestamp, None)
+            except ValueError:
+                pass
 
 
     def run(self):
         while self.goon:
-            file_events = self.selector.select()
+            if len(self.timers):
+                timeout = self.timers[0][0] - time.monotonic()
+                if timeout <= 0: timeout = 0
+            else:
+                timeout = None
 
+            file_events = self.selector.select(timeout)
+
+            # timer handling
+            timestamp = time.monotonic() + 0.05
+            while len(self.timers) and self.timers[0][0] < timestamp:
+                watcher = self.timers[0][1]
+                if watcher:
+                    watcher.callback(watcher, timestamp)
+
+                    if self.timers[0][1] and watcher.repeat:
+                        watcher.timestamp += watcher.repeat
+                        heapq.heappushpop(self.timers, (watcher.timestamp, watcher))
+                    else:
+                        heapq.heappop(self.timers)
+                else:
+                    heapq.heappop(self.timers)
+
+            # file handling
             for key, events in file_events:
                 watcher = key.data
                 watcher.callback(watcher, events)
@@ -79,7 +125,6 @@ def test():
             eventloop.unregister(watcher)
         if data == 6:
             eventloop.unregister(watcher)
-            eventloop.stop()
             return
 
         data = data + 1
@@ -87,12 +132,26 @@ def test():
         watcher.file.send(data)
 
 
+    def time_callback(watcher, stamp):
+        print("time", stamp)
+
+        watcher.count += 1
+        if watcher.count == 5:
+            eventloop.unregister(watcher)
+            eventloop.stop()
+
+
     eventloop = Eventloop()
+
     sock_a, sock_b = socket.socketpair()
     watcher_a = FileWatcher(sock_a, EVENT_READ, sock_callback)
     watcher_b = FileWatcher(sock_b, EVENT_READ, sock_callback)
     eventloop.register(watcher_a)
     eventloop.register(watcher_b)
+
+    timer = TimeWatcher(0, 1, time_callback)
+    timer.count = 0
+    eventloop.register(timer)
 
     sock_a.send(b'\x00')
     eventloop.run()
